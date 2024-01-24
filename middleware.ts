26c4b4600fc52bc82ea176c61df44edbd3481718
alissonnxavier <alissonalves29@gmail.com>
2024-01-24 12:03:32 -0300

    added ngrek

--- middleware.ts
+++ middleware.ts
@@ -4,13 +4,9 @@ import { authMiddleware } from "@clerk/nextjs";
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
-   /*  publicRoutes: [
-        "/",
+     publicRoutes: [
         "/api/webhooks(.*)",
-        "/api/uploadthing",
-        "/:username",
-        "/search"
-    ] */
+    ] 
 });
 
 export const config = {
