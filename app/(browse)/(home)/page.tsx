0e0b9308d215fde1acca8066643c2e8d52d5b608
alissonnxavier <alissonalves29@gmail.com>
2024-01-26 10:52:39 -0300

    created organizational folders

---
+++ app/(browse)/(home)/page.tsx
@@ -0,0 +1,22 @@
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { UserButton } from '@clerk/nextjs'
+
+export default function Home() {
+  return (
+    <>
+      <div className='text-red-600'>
+        Only authenticated users can see this
+        <Input />
+        <Button variant='destructive' size='icon'>
+          Hii
+        </Button>
+        <UserButton afterSignOutUrl='/' />
+
+        <div className='text-blue-700'>
+          <Input />
+        </div>
+        </div>
+      </>
+      )
+}
